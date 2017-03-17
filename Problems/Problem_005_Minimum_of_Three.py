546faaa9e4570b95ac7d3c9c624327996893c2a0
SergiiKrav4enko <sergiikrav4enko@gmail.com>
2017-03-17 18:30:24 +0200

    Problem_005

---
+++ Problems/Problem_005_Minimum_of_Three.py
@@ -0,0 +1,15 @@
+"""Даны несколько троек чисел. Требуется выбрать наименьшее в каждой тройке.
+
+Входные данные - первая строка указывает количество троек чисел.
+Каждая из следующих строк содержит одну такую тройку.
+Ответ должен содержать выбранные из каждой тройки минимумы, по порядку, через пробел."""
+n = int(input())
+min = 0
+for i in range(n):
+    line = input().split()
+    if int(line[0])<int(line[1]) and int(line[0])<int(line[2]):
+        print(line[0], end=' ')
+    if int(line[1])<int(line[0]) and int(line[1])<int(line[2]):
+        print(line[1], end=' ')
+    if int(line[2])<int(line[0]) and int(line[2])<int(line[1]):
+        print(line[2], end=' ')
